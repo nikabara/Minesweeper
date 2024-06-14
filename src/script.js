1b7d1035f9fb24006d099c01d73dbe8cd59a8d80
nikoloz baratashvili <n.baratashvili@itstep.ge>
2024-06-14 21:45:40 +0400

    Bomb randomizing algorithm done

---
+++ src/script.js
@@ -0,0 +1,40 @@
+// declarations
+let tiles = document.querySelectorAll(".box");
+
+let bombCount = 10;
+
+let tileMap = new Map();
+
+//main
+
+const SetBombs = (bombCount, bombSpawnDensity, bombSpawnFreq) => {
+    for (let i = 0; i < tiles.length; i++) {
+        let randomNum = Math.round(Math.random() * bombSpawnFreq);
+    
+        if (randomNum > bombSpawnDensity && bombCount > 0) {
+            tileMap.set(tiles[i].id, "Bomb");
+            bombCount--;
+        }
+        else {
+            tileMap.set(tiles[i].id, "Safe");
+        }
+    }
+}
+
+SetBombs(10, 8.5, 10);
+// SetBombs(10, 8, 10);
+
+for (let [key, value] of tileMap) {
+    switch (value) {
+        case "Bomb":
+            document.getElementById(key).classList.add("Bomb");
+            break;
+        case "Safe":
+            document.getElementById(key).classList.add("Hidden");
+            break;
+        default:
+            break;
+    }
+}
+
+console.log(tileMap);
