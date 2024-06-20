7484f307fd3491ddf4d176bef0fdf5d821cb7103
nikoloz baratashvili <n.baratashvili@itstep.ge>
2024-06-21 01:05:40 +0400

    Furthermore development to this game, added correct numbering on number spots and some bug fixes

--- src/script.js
+++ src/script.js
@@ -31,6 +31,25 @@ const SetBombs = (bombCount, bombSpawnDensity, bombSpawnFreq) => {
 // SetBombs(12, 8.5, 9.5);
 SetBombs(10, 8, 10);
 
+const BombCountToNumberImageClass = (bombCount) => {
+    switch (bombCount) {
+        case 1:
+            return "NumberOne";
+            
+        case 2:
+            return "NumberTwo";
+            
+        case 3:
+            return "NumberThree";
+        
+        case 4:
+            return "NumberFour";
+    
+        default:
+            break;
+    }
+}
+
 let i = 0; // Left
 let j = 2; // Right
 
@@ -87,6 +106,57 @@ for (let [key, value] of tileMap) {
     i++; j++;
 }
 
+const SetNumbers = (array) => {
+    let i = 0;
+    let j = 2;
+
+    let bombCount = 0;
+
+    for(let [key, value] of array) {
+        let leftElement = array.get(`box-${i}`);
+        let rightElement = array.get(`box-${j}`);
+        let topElement = array.get(`box-${i-8}`);
+        let bottomElement = array.get(`box-${i + 10}`);
+        let TopLeftElement = array.get(`box-${i - 9}`);
+        let TopRightElement = array.get(`box-${j - 9}`);
+        let BottomLeftElement = array.get(`box-${i + 9}`);
+        let BottomRightElement = array.get(`box-${j + 9}`);
+
+        if (document.getElementById(key).classList.contains("NumberSpot")) {
+            if (leftElement == "Bomb" && !document.getElementById(key).classList.contains("LEdge") && !document.getElementById(key).classList.contains("LBcorner")) {
+                bombCount++;
+            }
+            if (rightElement == "Bomb" && !document.getElementById(key).classList.contains("REdge") && !document.getElementById(key).classList.contains("RTcorner")) {
+                bombCount++;
+            }
+            if (topElement == "Bomb" && !document.getElementById(key).classList.contains("TEdge") && !document.getElementById(key).classList.contains("LTcorner") && !document.getElementById(key).classList.contains("RTcorner")) {
+                bombCount++;
+            }
+            if (bottomElement == "Bomb" && !document.getElementById(key).classList.contains("BEdge") && !document.getElementById(key).classList.contains("LBcorner") && !document.getElementById(key).classList.contains("RBcorner")) {
+                bombCount++;
+            }
+            if (TopLeftElement == "Bomb" && !document.getElementById(key).classList.contains("LEdge") && !document.getElementById(key).classList.contains("LBcorner")) {
+                // console.log(document.getElementById(`box-${i - 9}`).id, i)
+                bombCount++;
+            }
+            if (TopRightElement == "Bomb" && !document.getElementById(key).classList.contains("REdge") && !document.getElementById(key).classList.contains("RBcorner") && !document.getElementById(key).classList.contains("RTcorner")) {
+                // console.log(document.getElementById(`box-${j - 9}`).id, j);
+                bombCount++;
+            }
+            if (BottomLeftElement == "Bomb" && !document.getElementById(key).classList.contains("LEdge") && !document.getElementById(key).classList.contains("LBcorner")) {
+                // console.log(document.getElementById(`box-${i + 9}`).id, i);
+                bombCount++;
+            }
+            // 
+            if (BottomRightElement == "Bomb" && !document.getElementById(key).classList.contains("REdge") && !document.getElementById(key).classList.contains("RBcorner") && !document.getElementById(key).classList.contains("LBcorner")) {
+                bombCount++;
+            }
+            document.getElementById(key).classList.add(BombCountToNumberImageClass(bombCount));
+        }
+        bombCount = 0;
+        i++; j++;
+    }
+}
 
 
 
@@ -99,6 +169,21 @@ for (let [key, value] of tileMap) {
     else if (value === "Safe") {
         document.getElementById(key).classList.add("Hidden");
     }
+    document.getElementById(key).classList.add("Hidden");
+
 }
 
-console.log(tileMap);
+// console.log(tileMap);
+
+
+SetNumbers(tileMap);
+
+
+document.body.onclick = (item) => {
+    if (item.target.classList.contains("Hidden")) {
+        item.target.classList.remove("Hidden")
+    }
+    if (item.target.classList.contains("Bomb")) {
+        location.reload();
+    }
+}
